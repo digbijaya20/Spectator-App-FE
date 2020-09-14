4ca90fff78175e72edf72cc1d349719abca3c3a8
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2020-09-14 10:11:29 +0530

    added  guest context

--- src/Context/guestContext/GuestState.js
+++ src/Context/guestContext/GuestState.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+import GuestContext from './GuestContext';
+import GuestReducer from './GuestReducer';
 
-const GuestState = () => {
+const GuestState = (props) => {
+    const initialState = {
+        guests: [
+            {
+                id:1,
+                name:"ditu",
+                phone:73773737373,
+                dietary:"Non-Veg",
+                iscomfirmed:true
+            },
+            {
+                id:2,
+                name:"ditu",
+                phone:73773737373,
+                dietary:"Non-Veg",
+                iscomfirmed:true
+            },
+            {
+                id:3,
+                name:"ditu",
+                phone:7377373,
+                dietary:"Non-Veg",
+                iscomfirmed:false
+            },
+        ]
+    }
+const [state, dispatch] = useReducer(GuestReducer, initialState)
     return (
-        <div>
-            
-        </div>
+        <GuestContext.Provider
+        value = {{guests: state.guests}}
+        >
+            {props.children}
+        </GuestContext.Provider>
     )
 }
 
